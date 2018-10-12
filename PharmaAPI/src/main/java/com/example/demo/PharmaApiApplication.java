d4ef78b1a329f94f0e4126fe00036066d0de88e8
Dante <dante.usr@gmail.com>
2018-10-12 17:38:37 -0500

    Origin

---
+++ PharmaAPI/src/main/java/com/example/demo/PharmaApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmaApiApplication.class, args);
+	}
+}
